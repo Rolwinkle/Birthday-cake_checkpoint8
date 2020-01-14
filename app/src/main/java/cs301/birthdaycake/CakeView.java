a5700bda6cec22ba28fbabf840dc6dd8b571d36e
Rolwinkle <tucker77a>
2020-01-13 20:43:11 -0800

    Extra Candle

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -122,6 +122,9 @@ public class CakeView extends SurfaceView {
         //Now a candle in the center
         drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
 
+        //Now a candle on the right
+        drawCandle(canvas, cakeLeft + cakeWidth/4 - candleWidth/2, cakeTop);
+
     }//onDraw
 
 }//class CakeView
